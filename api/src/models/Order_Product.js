36129e7d9d1b4d4412bd0fcf18160b76a0fea01f
rodvla <67304782+rodvla@users.noreply.github.com>
2020-09-01 15:17:17 -0300

    base configurada

---
+++ api/src/models/Order_Product.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize')
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+	// defino el modelo
+	sequelize.define('order_product', {
+		quantity: {
+			type: DataTypes.INTEGER,
+			defaultValue: 0,
+			validate: {
+				min: 0,
+			},
+		}		
+	})
+}
