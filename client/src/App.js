9fd61795be575432855b06a8ee20c781ccd3ba1b
snc0lt <xjinxer@gmail.com>
2020-09-02 16:10:48 -0500

    inital commit Product details page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar.jsx';
 import Products from './components/Products/Products'
+import ProductDetails from './components/Products/ProductDetails';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 
@@ -10,29 +12,19 @@ function App() {
   //Se debe pasar en las props, una función al componente SearchBar para realizar la busqueda de
   //los productos, esta función la puse para que la recibiera el componente.
   //No cumple con su objetivo, por ahora.
-  const searchAll = function(searchField){
-    console.log("busqueda con el término: ",searchField);
-  }
+  // const searchAll = function(searchField){
+  //   console.log("busqueda con el término: ",searchField);
+  // }
   return (
     <div className="App">
-
-      <SearchBar search={searchAll}/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Products />
-
+      <Router>
+        {/* por los momentos se muestra solo el searchbar pero aqui debe ir un navbar */}
+        <SearchBar /> 
+        <Switch>
+          <Route exact path='/products' component={Products} />
+          <Route exact path='/products/:id' component={ProductDetails} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/Products/ProductDetails.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Rating from '@material-ui/lab/Rating';
+import Container from '@material-ui/core/Container';
+import iphoneImage from '../../testImages/iphone.jpeg'
+import Carousel from '../ImgProductCardCarousel/Carousel';
+import Copyright from '../utils/Copyright';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '80vh',
+  },
+  root2: {
+    marginTop: '15%',
+  },
+  image: {
+    backgroundImage: `url(${iphoneImage})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100%',
+    objectFit: 'contain',
+    paddingTop: '60%'
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    // alignItems: 'center',
+  },
+  buttons: {
+    margin: theme.spacing(8, 4),
+    // margin: 'auto',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+}));
+
+export default function ProductDetails() {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="md" >
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        {/* <Grid item xs={false} sm={6} md={7} className={classes.image}>
+          
+        </Grid> */}
+        <Grid item xs={false} sm={6} md={7}>
+          <div className={classes.paper}>
+          <Carousel />
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={6} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Typography component="h6" variant="h6">
+              Iphone 11 pro max
+            </Typography>
+            <div>
+              <Rating />
+            </div>
+          </div>
+          <div className={classes.paper}>
+            <Typography component="h4" variant="h4" color='primary'>
+              $1000.00
+          </Typography>
+            <Typography variant='subtitle2' color='textSecondary'>
+              Stock - Disponible
+          </Typography>
+          </div>
+          <div className={classes.paper}>
+            <Typography variant='body1' color='textPrimary'>
+              Color:  Negro
+          </Typography>
+            <Typography variant='subtitle2' color='textSecondary'>
+              <span>Cantidad</span>: (select) (...disponibles)
+          </Typography>
+          </div>
+          <div className={classes.buttons}>
+            <Button variant="contained" color="primary" size="small">
+              Buy
+          </Button>
+            <Button variant="outlined" color="primary" size='small' style={{ marginLeft: 'auto' }}>
+              add to cart
+          </Button>
+          </div>
+          <div className={classes.paper}>
+          <Copyright />
+          </div>
+        </Grid>
+      </Grid>
+      <Grid container component="main" className={classes.root2}>
+        <CssBaseline />
+        <Grid item xs={12} sm={12} md={12} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Typography component="h6" variant="h6">
+              ...Aqui esta mi description
+            </Typography>
+          </div>
+          <div className={classes.paper}>
+            <Typography variant='body1' color='textPrimary'>
+              Soy un celular copado, por ahi algunos dicen que no valgo lo que cuesto, pero al c#r#j# los haters...!
+            </Typography>
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
