3597f38ccd481f05584d6fc0899f0fab02414cea
Tomas <tomasbarcojo@gmail.com>
2020-09-24 20:39:14 -0300

    Cancel order and stock control

--- client/src/components/AdminPanel/StateDialog.js
+++ client/src/components/AdminPanel/StateDialog.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -9,6 +9,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import { cleanGuestOrder } from '../../actions';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -21,10 +22,23 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 export default function DialogSelect({state,orderId}) {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
   const [orderState, setOrderState] = useState(state);
+  const [data, setData] = useState(null)
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch(`http://localhost:3001/orders/${orderId}`)
+      const orderX = await data.json()
+      setData(orderX)
+      console.log(orderX)
+    }
+    fetchData()
+  }, [])
+
 
   const handleChange = (event) => {
     setOrderState(event.target.value);
@@ -32,17 +46,46 @@ export default function DialogSelect({state,orderId}) {
 
   const handleClickOpen = () => {
     setOpen(true);
+    console.log(data.products)
   };
 
+  // const fetchData = async () => {
+  //   const data = await fetch(`http://localhost:3001/orders/${orderId}`)
+  //   const orderX = await data.json()
+  //   setOrder(orderX)
+  //   // console.log(orderX)
+  // }
+  
   const handleClose = () => {
-    try{
+    if (orderState === 'cancelada') {
+      // const data = fetch(`http://localhost:3001/orders/${orderId}`)
+      // const orderX = data.json()
+      // setOrder(orderX)
+      // console.log(data)
+      data.products.map(prod => {
+        let newStock = prod.stock + prod.order_product.quantity
+        const product = {
+          stock: newStock,
+        }
+        console.log('nuevo stock StateDIalog: ', newStock)
+        try {
+          const updatedProduct = fetch(`http://localhost:3001/products/stock/${prod.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(product),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+        } catch (err) {console.log(err)}
+      })
+      try{
         fetch(`http://localhost:3001/orders/detail/${orderId}`,{
             method:'PUT',
             body:JSON.stringify({state:orderState}),
             headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
             credentials:'include',
         })
         .then(res=>res.json())
@@ -52,7 +95,27 @@ export default function DialogSelect({state,orderId}) {
     } catch(error){
         console.log(error)
     }
-  };
+    }
+    else {
+      try{
+          fetch(`http://localhost:3001/orders/detail/${orderId}`,{
+              method:'PUT',
+              body:JSON.stringify({state:orderState}),
+              headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+              credentials:'include',
+          })
+          .then(res=>res.json())
+          .then(data=>console.log(data))
+          .catch(e =>console.log(e))
+          setOpen(false);
+      } catch(error){
+          console.log(error)
+      }
+    };
+    }
 
   return (
     <div>
