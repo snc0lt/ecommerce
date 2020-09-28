d1bfed14c37c42d980660febb5139836da1db1da
Tomas <tomasbarcojo@gmail.com>
2020-09-28 01:51:21 -0300

    Fixed some spanglish

--- client/src/components/SignUp/Register.js
+++ client/src/components/SignUp/Register.js
@@ -4,7 +4,7 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
+import { Link } from "react-router-dom";
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -200,7 +200,7 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="/user/login" variant="body2">
+              <Link to="/user/login" >
                 ¿Ya tenes una cuenta? Inicia sesión
               </Link>
             </Grid>
